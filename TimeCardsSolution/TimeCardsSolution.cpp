14ad529b6499bd34233ed72f063881db1ee628db
Annie Bonavides-Aguilar <anniebonav@outlook.com>
2021-06-23 23:55:18 -0500

    More and more validations.

--- TimeCardsSolution/TimeCardsSolution.cpp
+++ TimeCardsSolution/TimeCardsSolution.cpp
@@ -87,46 +87,7 @@ bool isNumber(std::string number) {
     return validNumber; //This will return weather the input is a number or not
 }
 
-int validateTimeNumber(char numberKind) {
-    int max = 0, intNumber = 0;
-    std::string kind, number;
-    bool validatedNum = false;
-    if (numberKind == 'm') {
-        max = 60;
-        kind = "Minute";
-    }
-    else if (numberKind == 'h') {
-        max = 24;
-        kind = "Hour";
-    }
-    while (validatedNum == false) {
-        std::cout << "\nPlease enter the " << kind << ": ";
-        std::cin >> number;
-
-        if (isNumber(number) == true) {
-            intNumber = std::stoi(number);
-            if (intNumber >= 0 && intNumber < max) {
-                validatedNum = true;
-            }
-            else {
-                std::cout << "\nPlease write a number between 0 and " << max;
-            }
-        }
-
-    }
-    return intNumber;
-}
-
-void setTimeMinute(Time& time) {
-    int minute = validateTimeNumber('m');
-}
-
-void setTimeHour(Time& time) {
-    int hour = validateTimeNumber('h');
-}
-
 void useClasses() {
-    std::cout << "\nHi, welcome to my program.\n";
     Time t1;
     Time t2(44, 14);
 
@@ -188,8 +149,6 @@ void showUsersVector(std::vector<User*> &users, int usersNum) {
     for (int i = 0; i < usersNum; i++) {
         users[i]->show();
     }
-
-    users[0]->show();
 }
 
 /*
@@ -253,85 +212,239 @@ void fillUsersVector(std::string usersFile, std::vector<User*> &users, int users
 
 }
 
-int validateNumber(int min, int max, std::string numberType) { //Number type is in which metho dthe number is being asked
+/*
+
+What the boolean needsValidation sees is I want that number to go through validation or not. If that number is just
+a part of a class or something, tyhen I do not need a validation. If it is used to choose a function, then I will
+require a validation. I think I iwll also use this function (validate number) to validate the writen information
+(when creating an instance of an object, for example). We will see
+
+*/
+int validateNumber(int min, int max, std::string numberType, bool needsValidation) { //Number type is in which metho dthe number is being asked
     bool validNumber = false, validChoice; /*ValidNumber is for the first while, validChoice is for the second 
     one. Could I use recursive?? Or something better??*/
     std::string stringNumber, validateAction;
     int validIntNum;
-
     while (validNumber != true) {
         validChoice = false;
-        std::cout << "\nNumber: ";
-        std::cin >> stringNumber;
-        if (isNumber(stringNumber) == true) {
-            validIntNum = stoi(stringNumber);
-            if (validIntNum >= min && validIntNum <= max) {
-                std::cout << "This is the number you chose: " << validIntNum << "\nPlease enter 1 if you accept this information, 2 if you want to re-write it or 0 if you want to abort this action.\nChoice: ";
-                
-                while (validChoice != true) {
-                    std::cin >> validateAction;
-                    if (validateAction == "1") {
-                        std::cout << "\nThis is the number you chose: " << validIntNum;
-                        validChoice = true;
-                        validNumber = true;
-
-                    }else if (validateAction == "2") {
-                        std::cout << "\nYou have decided to write another number for the " << numberType;
-                        validChoice = true;
-
-                    }else if (validateAction == "0") {
-                        std::cout << "\nYou have decided to abort the " << numberType << " action.";
-                        validIntNum = -1000;
-                        validChoice = true;
-                        validNumber = true;
+        std::cout << "\nNumber: "; std::cin >> stringNumber;
+        if (isNumber(stringNumber) == true) { /* This checks that the input is a number (through isNumber function
+        which takes into account the input as a string. If it is a number, then the path continues. If it is not a
+        number, then it just leaves the if and prints that 'no special characters or letters are allowed'.*/
+            
+            validIntNum = stoi(stringNumber); /*As I am now sure that the input is a number, then I can convert it
+            to an integer with string to internet method. I do this making an integer variable 'validIntNum' to the result of
+            stoi(stringNumber) value. With now I can now compare the integer to the minimum and maximum values.*/
+            
+            if (validIntNum >= min && validIntNum <= max) {/*This if checks that the value inserted (which we now
+            are sure that is an integer) is greater or equal than the established minimum and lower or equal than
+            the established maximum. If it checks the criteria, then we will ocntinue the path; If it does not,
+            then */
+                if (needsValidation == true) {
+                    std::cout << "\nPlease enter 1 if you accept this information, 2 if you want to re-write it or 0 if you want to abort this action.\nChoice: ";
+                    while (validChoice != true) {
+                        std::cin >> validateAction;
+                        if (validateAction == "1") {
+                            std::cout << "\nThis is the number you chose: " << validIntNum << std::endl;
+                            validChoice = true;
+                            validNumber = true;
+                        } else if (validateAction == "2") {
+                            std::cout << "\nYou have decided to write another number for the " << numberType;
+                            validChoice = true;
+                        } else if (validateAction == "0") {
+                            validIntNum = -1000;
+                            validChoice = true;
+                            validNumber = true;
+                        }
                     }
+                } else { //Needs validation == false
+                    std::cout << "This is the number you chose: " << validIntNum;
+                    validNumber = true;
                 }
-                
+            } else { // validNum meets criteria
+                std::cout << "\nRemember that your number must be greater or equal than " << min << " and lower or equal than " << max << ".";
             }
+        } else { //Number is integer
+            std::cout << " '" << stringNumber << "' is not a number.\n";
         }
-
     }
     return validIntNum;
 }
 
-void createUser(std::vector<User*> &users) {
+char validateChar() {
+    std::string myStringChar;
+    char myChar;
+    std::cout << "\nPlease enter your char: ";
+    std::cin >> myStringChar;
+    myChar = myStringChar[0];
+
+    return myChar;
+}
+
+/*
+
+SET FUNCTIONS
+
+*/
+void setTimeMinute(Time &time) {
+    int minute = validateNumber(0, 59, "minute", true);
+    if (minute == -1000) {
+        std::cout << "You have decided to stop setting a Time's Minute";
+    }else {
+        time.setMinute(minute);
+    }
+}
+
+void setTimeHour(Time& time) {
+    int hour = validateNumber(1, 24, "minute", true);
+    if (hour == -1000) {
+        std::cout << "You have decided to stop setting the Time's Hour";
+    } else {
+        time.setMinute(hour);
+    }
+}
+
+
+/*
+
+CREATE FUNCTIONS
+
+*/
+
+Time createTime() {
+    int minute, hour;
+    std::string kind, number;
+    bool validatedNum = false;
+    minute = validateNumber(0, 59, "minute", false);
+    hour = validateNumber(1, 24, "hour", false);
+
+    Time time(hour, minute);
+
+    return time;
+}
+
+
+void createStudent(std::vector<User*>& users, std::string userId, std::string userName, int userAge) {
+    std::string userType = "s", studentProgramName = "";
+    char studentStudyType = ' ';
+    
+    studentStudyType = validateChar();
+    std::cout << "Program Name: ";
+    std::cin >> studentProgramName;
+    
+    users.push_back(new Student(userId, userName, userAge, studentStudyType, studentProgramName));
+}
+
+/*
+
+This function creates an intern using as base the information recollected from both the userCreation method and
+the workerCreation one.
+
+*/
+void createIntern(std::vector<User*>& users, std::string userId, std::string userName, int userAge, std::string workerWorkArea, float workerSalary, std::string workerType) {
+    std::string internType;
+    int internDuration;
+
+    std::cout << "Intern Type: "; std::cin >> internType;
+    std::cout << "Please enter the Internship duration (in months)";
+    internDuration = validateNumber(0, 12, "internship duration (in months)", false);
+
+    users.push_back(new Intern(userId, userName, userAge, workerWorkArea, workerSalary, internType, internDuration));
+}
+
+void createFTW(std::vector<User*>& users, std::string userId, std::string userName, int userAge, std::string workerWorkArea, float workerSalary, std::string workerType) {
+    std::string FTWtype;
+    int yearsWorked;
+    std::cout << "Full Time Worker Type: "; std::cin >> FTWtype;
+    std::cout << "Please enter how long this FTW has worked (in years)";
+    yearsWorked = validateNumber(0, 80, "internship duration (in months)", false); /*I oculd create a method that checks
+    that the years worked, minus 18, is not bigger than the worker's age.*/
+    users.push_back(new Intern(userId, userName, userAge, workerWorkArea, workerSalary, FTWtype, yearsWorked));
+}
+
+void createWorker(std::vector<User*>& users, std::string userId, std::string userName, int userAge) {
     int choice;
-    std::string userType;
-    std::cout << "\nChoose which Type of User will it be:\n1) Worker\n2) Student\n3) Unspecified\n0) Stop Action";
-    choice = validateNumber(0, 3, "'choosing user type'");
+    std::string userType = "w", workerWorkArea = "", workerType = "";
+    float workerSalary = 0;
+
+    std::cout << "\n\nChoose which type of Worker will it be:\n1) Intern\n2) Full Time Worker\n3) Unspecified\n0) Stop Action";
+    choice = validateNumber(0, 3, "'choosing Worker type'", true);
+    if (choice == 0) {
+        std::cout << "\nYou have chosen to stop the action.";
+    }else {
+        std::cout << "Work Area: "; std::cin >> workerWorkArea;
+        std::cout << "Salary: "; std::cin >> workerSalary;
+        switch (choice) {
+            case 1:
+                userType = "i";
+                createIntern(users, userId, userName, userAge, workerWorkArea, workerSalary, workerType);
+                break;
+            case 2:
+                userType = "f";
+                createFTW(users, userId, userName, userAge, workerWorkArea, workerSalary, workerType);
+                break;
+            case 3:
+                userType = "w";
+                users.push_back(new Worker(userId, userName, userAge, workerWorkArea, workerSalary));
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+void createUser(std::vector<User*> &users) {
+    int choice, userAge;
+    std::string userId, userName;
+    std::cout << "\n\nENTER YOUR USER'S INFORMATION";
+    std::cout << "\nUser Id: ";
+    std::cin >> userId;
+    std::cout << "Name: ";
+    std::cin >> userName;
+    std::cout << "Please enter your age information";
+
+    userAge = validateNumber(0, 120, "user Age", false);
+
+    std::cout << "\n\nChoose which Type of User will it be:\n1) Worker\n2) Student\n3) Unspecified\n0) Stop Action";
+    choice = validateNumber(0, 3, "'choosing user type'", true);
     switch (choice) {
         case 0:
-            std::cout << "\nChoose which type of Worker will it be:\n1) Intern\n2) Full Time Worker\n3) Unspecified\n0) Stop Action";
-            choice = validateNumber(0, 3, "'choosing Worker type'");
-            switch (choice) {
-                case 0:
-                    userType = "i";
-                    break;
-                case 1:
-                    userType = "f";
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                default:
-                    break;
-            }
-            break;
-
+            std::cout << "\nYou decided to abort the action.";
         case 1:
-            userType = "s";
+            createWorker(users, userId, userName, userAge);
             break;
         case 2:
-            userType = "u";
+            createStudent(users, userId, userName, userAge);
             break;
+        case 3:
+            users.push_back(new User(userId, userName, userAge));
         default:
             break;
     }
 }
 
-void createTimeCard(std::vector<TimeCard*>timeCards) {
+void createTimeCard(std::vector<TimeCard*>timeCards, int timeCardsNum) {
+    std::string userId;
 
+    std::cout << "\n\nENTER YOUR TIME CARD'S INFORMATION";
+    //Time Card INdex is assigned automatically as the next number in the timeCards Vector
+    std::cout << "\nUser Id: "; std::cin >> userId;
+
+    //I can simplify this by creating a "Time Creator" and then just use the other TimeCard constructor
+    std::cout << "Please enter your Initial Time information";
+    Time t1 = createTime();
+    if (t1.getMinute() == -1000 || t1.getHour() == -1000) {
+        std::cout << "You have decided to abort creating a time Card";
+    } else {
+        std::cout << "Please enter your Final Time information";
+        Time t2 = createTime();
+
+        if (t2.getMinute() == -1000 || t2.getHour() == -1000) {
+            std::cout << "You have decided to abort creating a time Card";
+        } else {
+            timeCards.push_back(new TimeCard(timeCardsNum, userId, t1, t2));
+        }
+    }
 }
 
 
@@ -342,20 +455,23 @@ void menu(std::string usersFile, std::string timeCardsFile, std::vector<User*> &
     std::cout << "\nHey! Thanks for using this program!\n";
     while (activeMenu != false) {
         std::cout << divider << "Here is the menu you can choose from.\n1) Create User\n2) Create Time Card\n3) See Users\n4) See Time Cards\n0) Exit" << divider;
-        choice = validateNumber(0, 4, "'choosing menu function'");
+        choice = validateNumber(0, 4, "'choosing menu function'", true);
         switch (choice) {
             case 0:
-                std::cout << "\n\nYou have chosen to exit the menu. Thanks for comming!\n\n";
+                std::cout << divider << "You have chosen to exit the menu. Thanks for comming!" << divider << std::endl;
                 activeMenu = false;
                 break;
             case 1:
-                std::cout << divider << "YOU HAVE CHOSEN TO ADD A USER\n";
+                std::cout << divider << "YOU HAVE CHOSEN TO ADD A USER";
                 createUser(users);
+                usersNum += 1;
                 break;
                 std::cout << divider;
             case 2:
-                std::cout << "YOU HAVE CHOSEN TO ADD A NEW TIME CARD\n";
-                createTimeCard(timeCards);
+                std::cout << divider << "YOU HAVE CHOSEN TO ADD A NEW TIME CARD";
+                createTimeCard(timeCards, timeCardsNum);
+                timeCardsNum += 1;
+
                 break;
             case 3:
                 std::cout << "\nYOU HAVE CHOSEN TO SEE THE USERS";
@@ -402,16 +518,3 @@ int main() {
 
     return 0;
 }
-
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
